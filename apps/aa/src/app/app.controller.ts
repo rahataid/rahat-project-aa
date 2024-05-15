0905b34854baca3228383a0b7f75489b1b35d840
avash <avash.ghimire@rumsan.net>
2024-05-15 16:44:51 +0545

    update: aa specific settings

--- apps/aa/src/app/app.controller.ts
+++ apps/aa/src/app/app.controller.ts
@@ -1,13 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { JOBS } from '../constants';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @MessagePattern({ cmd: JOBS.SETTINGS.CREATE, uuid: process.env.PROJECT_ID })
+  addSettings(dto: any) {
+    return this.appService.addSettings(dto);
+  }
+
+  @MessagePattern({ cmd: JOBS.SETTINGS.LIST, uuid: process.env.PROJECT_ID })
+  listSettings() {
+    return this.appService.listSettings()
+  }
+
+  @MessagePattern({ cmd: JOBS.SETTINGS.GET, uuid: process.env.PROJECT_ID })
+  getSettings(dto: any) {
+    return this.appService.getSettings(dto)
   }
 }
--- apps/aa/src/app/app.service.ts
+++ apps/aa/src/app/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { SettingsService } from '@rumsan/settings';
+import { lowerCaseObjectKeys } from '../utils/utility';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly settingService: SettingsService) {
+    this.refreshSettings();
+  }
+
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async addSettings(dto: any) {
+    return this.settingService.create(dto);
+  }
+
+  async listSettings() {
+    const res = await this.settingService.listAll();
+    const lowerCaseRes = lowerCaseObjectKeys(res)
+    return lowerCaseRes;
+
+  }
+  async getSettings(dto: any) {
+    const { name } = dto
+    const res = await this.settingService.getPublic(name);
+    return lowerCaseObjectKeys(res);
+  }
+
+  async refreshSettings() {
+    const d = await this.settingService.listPublic();
+    require('./settings.config').setSettings(d);
+  }
+
+  static generateMessagePattern(patternPrefix: string) {
+    const settings =
+      require('./settings.config').getSettings('PROJECT_SETTINGS');
+    return { cmd: patternPrefix, uuid: settings.UUID || '' };
   }
 }
