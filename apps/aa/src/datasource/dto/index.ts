bd2b1702179ea0153e7fe88c4fa3898e9bfcce02
avash <avash.ghimire@rumsan.net>
2024-06-05 21:48:54 +0545

    update: glofas dto

--- apps/aa/src/datasource/dto/index.ts
+++ apps/aa/src/datasource/dto/index.ts
@@ -19,3 +19,30 @@ export interface DhmDataObject {
   description: string;
   station: number;
 }
+
+export interface GlofasStationInfo {
+  LOCATION: string;
+  I: string;
+  J: string;
+  URL: string;
+  BBOX: string;
+  TIMESTRING: string;
+}
+
+export interface GlofasDataObject {
+  pointForecastData: {
+    forecastDate: string;
+    maxProbability: string;
+    alertLevel: string;
+    maxProbabilityStep: string;
+    dischargeTendencyImage: string;
+    peakForecasted: string;
+  };
+  hydrographImageUrl: string;
+  returnPeriodData: Array<{
+    forecastDay: string;
+    forecastDate: string;
+    forecastData: string;
+  }>;
+  forecastDate: string;
+}
