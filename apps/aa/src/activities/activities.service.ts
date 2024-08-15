35c3cba17a73f28a7c175d68c8cf55de039305cb
avash <avash.ghimire@rumsan.net>
2024-08-15 17:50:50 +0545

    send session id in fetch one

--- apps/aa/src/activities/activities.service.ts
+++ apps/aa/src/activities/activities.service.ts
@@ -175,6 +175,9 @@ export class ActivitiesService {
           groupName: groupName,
           transportName: transportName,
           sessionStatus,
+          ...(communication.sessionId && {
+            sessionId: communication.sessionId,
+          }),
         });
       }
     }
