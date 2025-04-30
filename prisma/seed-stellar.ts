b9bbf905f247520de4748a74242b48e58bb69288
Sushant Tripathu <sushant@RS-L-25-02-Macair.local>
2025-04-30 17:42:40 +0545

    stellar seed updates

--- prisma/seed-stellar.ts
+++ prisma/seed-stellar.ts
@@ -7,21 +7,54 @@ const prismaService = new PrismaService();
 const settings = new SettingsService(prismaService);
 
 const main = async () => {
-  const stellarValue = {
-    tenantName: 'sandab',
-    server: 'https://soroban-testnet.stellar.org',
-    keypair: 'SAKQYFOKZFZI2LDGNMMWN3UQA6JP4F3JVUEDHVUYYWHCVQIE764WTGBU',
-    email: 'owner@sandab.stellar.rahat.io',
-    password: 'Password123!',
-    contractId: 'CARWC3PLXU7V6EPZ67GDIDIU2ZSXN7ASHSX35G4THJHTVT2ZQY232KN2',
-    vendorAddress: 'GBNYMT3TSPAV5BMPW6QNRZYXN5U3BMDAZE54O7AWGIIZ5CUDIA7RYAEB',
-  };
+  const activeYear = process.env.ACTIVE_YEAR;
+  const riverBasin = process.env.RIVER_BASIN;
+  if (!activeYear || !riverBasin) {
+    throw new Error(
+      'ACTIVE_YEAR and RIVER_BASIN environment variables are required'
+    );
+  }
 
-  await settings.create({
-    name: 'STELLAR_SETTINGS',
-    value: stellarValue,
-    isPrivate: false,
-  });
+  try {
+    const dataSource = await settings.getPublic('STELLAR_SETTINGS');
+
+    if (dataSource) {
+      console.log('PROJECTINFO already exists');
+      await settings.delete('STELLAR_SETTINGS');
+      console.log('Old PROJECTINFO deleted');
+    }
+    const stellarValue = {
+      tenantName: 'sandab',
+      server: 'https://soroban-testnet.stellar.org',
+      keypair: 'SAKQYFOKZFZI2LDGNMMWN3UQA6JP4F3JVUEDHVUYYWHCVQIE764WTGBU',
+      email: 'owner@sandab.stellar.rahat.io',
+      password: 'Password123!',
+      contractId: 'CARWC3PLXU7V6EPZ67GDIDIU2ZSXN7ASHSX35G4THJHTVT2ZQY232KN2',
+      vendorAddress: 'GBNYMT3TSPAV5BMPW6QNRZYXN5U3BMDAZE54O7AWGIIZ5CUDIA7RYAEB',
+    };
+
+    await settings.create({
+      name: 'STELLAR_SETTINGS',
+      value: stellarValue,
+      isPrivate: false,
+    });
+  } catch (error) {
+    const stellarValue = {
+      tenantName: 'sandab',
+      server: 'https://soroban-testnet.stellar.org',
+      keypair: 'SAKQYFOKZFZI2LDGNMMWN3UQA6JP4F3JVUEDHVUYYWHCVQIE764WTGBU',
+      email: 'owner@sandab.stellar.rahat.io',
+      password: 'Password123!',
+      contractId: 'CARWC3PLXU7V6EPZ67GDIDIU2ZSXN7ASHSX35G4THJHTVT2ZQY232KN2',
+      vendorAddress: 'GBNYMT3TSPAV5BMPW6QNRZYXN5U3BMDAZE54O7AWGIIZ5CUDIA7RYAEB',
+    };
+
+    await settings.create({
+      name: 'STELLAR_SETTINGS',
+      value: stellarValue,
+      isPrivate: false,
+    });
+  }
 };
 
 main()
