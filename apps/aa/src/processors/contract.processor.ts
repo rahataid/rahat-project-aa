b9ff3f3042ca3b5ee0d40d9d5a9f177d4aed7892
Sushant Tripathu <sushant@RS-L-25-02-Macair.local>
2026-01-09 11:59:40 +0545

    decimal fixes

--- apps/aa/src/processors/contract.processor.ts
+++ apps/aa/src/processors/contract.processor.ts
@@ -5,6 +5,7 @@ import { Job } from 'bull';
 import { PrismaService } from '@rumsan/prisma';
 import { JsonRpcProvider, ethers } from 'ethers';
 import { BeneficiaryService } from '../beneficiary/beneficiary.service';
+import { lowerCaseObjectKeys } from '../utils/utility';
 
 type IStringArr = string[];
 type ICallData = IStringArr[];
@@ -277,6 +278,31 @@ const ABI = [
       },
     ],
     name: 'TokenTransfer',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'beneficiary',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'vendor',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'TokenTransferred',
     type: 'event',
   },
   {
@@ -648,6 +674,29 @@ const ABI = [
     type: 'function',
   },
   {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_benAddress',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: '_vendorAddress',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: '_amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transferTokenToVendor',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
     inputs: [],
     name: 'trustedForwarder',
     outputs: [
@@ -688,16 +737,41 @@ export class ContractProcessor {
       );
 
       const multicallTxnPayload = [];
+
+      const contract = await this.getFromSettings('CONTRACT');
+      const formatedAbi = lowerCaseObjectKeys(contract.RAHATTOKEN.ABI);
+
+      const chainConfig = await this.getFromSettings('CHAIN_SETTINGS');
+
+      const rpcUrl = chainConfig.rpcUrl;
+      const provider = new ethers.JsonRpcProvider(rpcUrl);
+
+      const rahatTokenContract = new ethers.Contract(
+        contract.RAHATTOKEN.ADDRESS,
+        formatedAbi,
+        provider
+      );
+
+      const decimal = await rahatTokenContract.decimals.staticCall();
+
       for (const benf of benfs) {
         if (benf.benTokens) {
-          multicallTxnPayload.push([benf.walletAddress, benf.benTokens]);
+          const formattedAmountBn = ethers.parseUnits(
+            benf.benTokens.toString(),
+            decimal
+          );
+
+          this.logger.log(
+            `Contract process: Converting amount ${benf.benTokens} to formatted amount ${formattedAmountBn} for beneficiary ${benf.walletAddress} using decimal ${decimal}`
+          );
+          multicallTxnPayload.push([benf.walletAddress, formattedAmountBn]);
         }
       }
 
-      const {
-        contract: aaContract,
-      } = await this.createContractInstanceSign('AAPROJECT');
-    
+      const { contract: aaContract } = await this.createContractInstanceSign(
+        'AAPROJECT'
+      );
+
       const txn = await this.multiSend(
         aaContract,
         'assignTokenToBeneficiary',
@@ -716,7 +790,7 @@ export class ContractProcessor {
     //  get RPC URL
     const res = await this.prisma.setting.findFirstOrThrow({
       where: {
-        name: 'BLOCKCHAIN',
+        name: 'CHAIN_SETTINGS',
       },
       select: {
         name: true,
@@ -740,6 +814,25 @@ export class ContractProcessor {
       provider,
       wallet,
     };
+  }
+
+  private async getFromSettings(key: string): Promise<any> {
+    try {
+      const settings = await this.prisma.setting.findUnique({
+        where: {
+          name: key,
+        },
+      });
+
+      if (!settings?.value) {
+        throw new Error(`${key} not found`);
+      }
+
+      return settings.value;
+    } catch (error) {
+      this.logger.error(`Error getting setting ${key}: ${error.message}`);
+      throw error;
+    }
   }
 
   async getContractByName(contractName: string) {
