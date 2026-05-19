7a332c9df730f4d4e51fb57f6d4143909e617f25
subash-rumsan <subhash.parajuli@rumsan.net>
2026-05-19 17:01:26 +0545

    fix: update concurrency for sending buld sms to benf

--- apps/aa/src/processors/inkind.processor.ts
+++ apps/aa/src/processors/inkind.processor.ts
@@ -14,7 +14,7 @@ export class InkindProcessor {
     private readonly prisma: PrismaService
   ) {}
 
-  @Process({ name: JOBS.INKINDS.SEND_BENEFICIARY_OTP_ON_QUEUE, concurrency: 5 })
+  @Process({ name: JOBS.INKINDS.SEND_BENEFICIARY_OTP_ON_QUEUE, concurrency: 1 })
   async handleSendBeneficiaryOtp(job: Job<{ phone: string }>) {
     const { phone } = job.data;
 
@@ -22,32 +22,39 @@ export class InkindProcessor {
       `Processing job to send OTP to beneficiary with phone: ${phone}`
     );
 
-    // verify phone number is valid or not
-    const { success, isValid } = await this.isValidNepaliNumber(phone);
-    if (!success) {
+    try {
+      // verify phone number is valid or not
+      const { success, isValid } = await this.isValidNepaliNumber(phone);
+      if (!success) {
+        this.logger.error(
+          `Failed to validate phone number: ${phone}. Skipping OTP sending.`
+        );
+        return;
+      }
+
+      if (!isValid) {
+        this.logger.warn(
+          `Invalid Nepali phone number: ${phone}. Skipping OTP sending.`
+        );
+        return;
+      }
+      // get master opt of that benf
+      const defaultOpt = await this.prisma.otp.findUnique({
+        where: { phoneNumber: phone },
+      });
+
+      // send otp to beneficiary phone number
+      return this.otpService.sendSms(
+        phone,
+        'Your OTP for inkind redemption is:',
+        defaultOpt?.otp
+      );
+    } catch (error) {
       this.logger.error(
-        `Failed to validate phone number: ${phone}. Skipping OTP sending.`
+        `Error processing OTP job for phone ${phone}: ${error.message}`
       );
-      return;
+      throw error;
     }
-
-    if (!isValid) {
-      this.logger.warn(
-        `Invalid Nepali phone number: ${phone}. Skipping OTP sending.`
-      );
-      return;
-    }
-    // get master opt of that benf
-    const defaultOpt = await this.prisma.otp.findUnique({
-      where: { phoneNumber: phone },
-    });
-
-    // send otp to beneficiary phone number
-    return this.otpService.sendSms(
-      phone,
-      'Your OTP for inkind redemption is:',
-      defaultOpt?.otp
-    );
   }
 
   async isValidNepaliNumber(
