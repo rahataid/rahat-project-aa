1172fbfcff995814591abb4dc13d068418f19e2b
manjik-rumsan <manjik.shrestha@rumsan.com>
2026-06-30 10:01:22 +0545

    removed stellar module

--- apps/aa/src/stellar/dto/fund-account.dto.ts
+++
@@ -1,4 +0,0 @@
-export class FundAccountDto {
-  walletAddress: string;
-  secretKey: string;
-} 
--- apps/aa/src/stellar/dto/send-asset-to-vendor.dto.ts
+++
@@ -1,6 +0,0 @@
-export class SendAssetToVendorDto {
-  phoneNumber: string;
-  otp: string;
-  receiverAddress: string;
-  amount: string;
-} 
