1bb9a750d9feb12eea36d07de38a584b911d911f
subash-rumsan <subhash.parajuli@rumsan.net>
2026-07-10 12:38:12 +0545

    fix: reoved validation where token is already reserved

--- apps/aa/src/beneficiary/beneficiary.service.ts
+++ apps/aa/src/beneficiary/beneficiary.service.ts
@@ -629,17 +629,17 @@ export class BeneficiaryService {
       `Reserving ${totalTokensReserved} tokens for group: ${beneficiaryGroupId}`
     );
 
-    const isAlreadyReserved =
-      await this.prisma.beneficiaryGroupTokens.findUnique({
-        where: { groupId: beneficiaryGroupId },
-      });
+    // const isAlreadyReserved =
+    //   await this.prisma.beneficiaryGroupTokens.findUnique({
+    //     where: { groupId: beneficiaryGroupId },
+    //   });
 
-    if (isAlreadyReserved) {
-      this.logger.warn(
-        `Token already reserved for group: ${beneficiaryGroupId}`
-      );
-      throw new RpcException('Token already reserved.');
-    }
+    // if (isAlreadyReserved) {
+    //   this.logger.warn(
+    //     `Token already reserved for group: ${beneficiaryGroupId}`
+    //   );
+    //   throw new RpcException('Token already reserved.');
+    // }
 
     const benfGroup = await this.prisma.beneficiaryGroups.findUnique({
       where: {
