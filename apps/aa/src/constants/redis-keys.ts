3b4fb0f7c305f942605b6f6f7de2bcd4ee770d25
Akash-rumsan <akash.rumsan@gmail.com>
2026-09-09 16:03:59 +0545

    keys shifted to redis keys constant

--- apps/aa/src/constants/redis-keys.ts
+++ apps/aa/src/constants/redis-keys.ts
@@ -1,5 +1,7 @@
+// Redis cache keys and TTLs for disbursement operations
 export const DISBURSE_CACHE_TTL = 300;
 export const DISBURSE_CACHE_KEY_PREFIX = 'disburse:progress:';
 
+// Redis cache keys and TTLs for group cash transfer operations
 export const GCT_CACHE_TTL = 300;
 export const GCT_CACHE_KEY_PREFIX = 'gct:progress:';
